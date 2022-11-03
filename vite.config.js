4ae290e1303ee98792efc8d11dbbf06440fb3296
QuachHuyThinh-B1910454 <quachhuythinh122001@gmail.com>
2022-11-03 13:37:05 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,7 +9,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
+  server:{
     port: 3001,
-  },
-})
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+    }
+  }
+});
